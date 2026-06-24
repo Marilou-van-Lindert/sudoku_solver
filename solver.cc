cd5e56ba04d390ad81adaabb25f77412b969b448
marilou.vanlindert@student.hu.nl <marilou.vanlindert@student.hu.nl>
2026-06-24 22:48:12 +0200

    made human solver

--- solver.cc
+++ solver.cc
@@ -11,34 +11,105 @@ bool Sudoku::FileAllowed(){
     return true;
 }
 
-
-//function fills the grind from sudoku.txt file
-void Sudoku::FillGrid (){
-    SudokuFile.get(number);
+bool Sudoku::Solved(){
     for (int i=0; i<sHeight; i++){
         for (int j=0; j<sWidth; j++){
-            grid[i][j]=number-'0';
-            SudokuFile.get(number);
+            if(grid[i][j]==0){
+                return false;
+            }
         }
     }
-    SudokuFile.close();
+    return true;
+}
+
+bool Sudoku::LegalNumber(int num){
+    if (num < 1 || num > 9){
+        return false;
+    }
+    return true;
+}
+
+bool Sudoku::LegalRow(int num){
+    if (num < 0 || num > sWidth){
+        return false;
+    }
+    return true;
+}
+
+bool Sudoku::LegalCol(int num){
+    if (num < 0 || num > sHeight){
+        return false;
+    }
+    return true;
 }
 
 
-bool Sudoku::NumberAllowed(int num, int row, int col){
+void Sudoku::FillNumber(){
+    AskNumber();
+    if(NumberAllowed()){
+        grid[ChosenRow][ChosenCol]=ChosenNumber;
+        ChosenNumber = NotLegal;
+        ChosenCol = NotLegal;
+        ChosenRow = NotLegal;
+    }
+
+}
+
+
+//function fills the grind from sudoku.txt file
+void Sudoku::FillGrid (){
+    //gets first character from document
+    SudokuFile.get(number);
+    //for each square fill it with a number
+    for (int i=0; i<sHeight; i++){
+        for (int j=0; j<sWidth; j++){
+            //checks if number is a number
+            if(number >= '0' && number <= '9'){
+                grid[i][j]=number-'0';
+            }
+            //get the next character
+            SudokuFile.get(number);
+        }
+    }
+    //close the file
+    SudokuFile.close();
+}
+
+void Sudoku::AskNumber(){
+    while (!LegalNumber(ChosenNumber)){
+        cout << "what number do you want:  ";
+        cin >> ChosenNumber; 
+        cout << "you chose number: " << ChosenNumber << endl;
+    }
+    while (!LegalRow(ChosenRow)){
+        cout << "what row do you want:  ";
+        cin >> ChosenRow; 
+        cout << "you chose row: " << ChosenRow << endl;
+        ChosenRow--;
+    }
+    while (!LegalCol(ChosenCol)){
+        cout << "what column do you want:  ";
+        cin >> ChosenCol; 
+        cout << "you chose column: " << ChosenCol << endl;
+        ChosenCol--;
+    }
+}
+
+
+
+bool Sudoku::NumberAllowed(){
     //checks if number is allowed in row
-    for (int i = 0, i<sWidth; i++){
-        if (grid[row][i]==num){
-            cout << "number not allowed"<< endl;
+    for (int i = 0; i<sWidth; i++){
+        if (grid[ChosenRow][i]==ChosenNumber){   
+            cout << "number not allowed in row"<< endl;
             return false;
         }
     }
 
     //checks if number is allowed in column
-
     for (int i = 0; i<sHeight; i++){
-        if (grid[i][col]==num){
-            cout << "number not allowed" << endl;
+        if (grid[i][ChosenCol]==ChosenNumber){
+            cout << "number not allowed column" << endl;
             return false;
         }
     }
@@ -47,9 +118,18 @@ bool Sudoku::NumberAllowed(int num, int row, int col){
     //steps get into the square
     //write code to read the square
     
+    int colStart = ChosenCol - ChosenCol%3;
+    int rowStart = ChosenRow - ChosenRow%3;
 
-
-
+    for (int i = 0; i<3;i++){
+        for (int j= 0; j<3; j++){
+            if(grid[rowStart+i][colStart+j]==ChosenNumber){
+                cout << "number not allowed box" << endl;
+                return false;
+            }
+        }
+    }
+    
 
     return true;
 }
@@ -77,5 +157,9 @@ int main (){
     Sudoku test;
     test.FillGrid();
     test.PrintSudoku();
+    while (!test.Solved()){
+        test.FillNumber();
+        test.PrintSudoku();
+    }
     return 0;
 }
--- solver.h
+++ solver.h
@@ -6,17 +6,28 @@ using namespace std;
 const int sHeight = 9;
 const int sWidth = 9;
 
+
 class Sudoku {
     private:
-        int grid[sHeight][sWidth]={-1};
-        // array [row][column]
+        int NotLegal = -1;
+        int grid[sHeight][sWidth]={NotLegal};
         char number;
         ifstream SudokuFile;
     public:
+        int ChosenNumber = NotLegal;
+        int ChosenRow = NotLegal;
+        int ChosenCol = NotLegal;   
         bool FileAllowed();
         void FillGrid();
         void PrintSudoku();
-        bool NumberAllowed(int num, int col, int row);
+        bool NumberAllowed();
+        void AskNumber();
+        bool LegalNumber(int num);
+        bool LegalRow(int num);
+        bool LegalCol(int num);
+        bool Solved();
+        void FillNumber();
+
         Sudoku()
         {
             SudokuFile.open("sudoku.txt");
